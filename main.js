932aaaf1cb19121888e949f28bc90f871b26e263
Bruno Pergentino <brunovpergentino@gmail.com>
2022-09-22 07:15:55 -0300

    small format change

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ function HidebothForm(){
 }
 function fun1() {
 
-    var e = document.getElementById('methods');
-    var selectedOption = e.options[e.selectedIndex].value;
+    const e = document.getElementById('methods');
+    const selectedOption = e.options[e.selectedIndex].value;
     if(selectedOption == "menst")
           {
            document.getElementById("ultrasound").style.visibility = "hidden";
@@ -34,14 +34,14 @@ HidebothForm();
 const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
 function calculate(){
     const yourSelect = document.getElementById( "methods" );
+    const today = new Date().getTime() //gets today's date and transforms into milliseconds
+    const todayAbs = Math.abs(today)
+
     if(yourSelect.options[ yourSelect.selectedIndex ].value == "menst"){
         const inputMenstDate = document.getElementById('dateMenst').value //get input from html
         const dateMenst = new Date(inputMenstDate).toUTCString() //transform input into midnight on the day picked
         const utcInput = new Date(dateMenst).getTime()//transforms date into milliseconds 
-        const today = new Date().getTime() //gets today's date and transforms into milliseconds
-        const todayAbs = Math.abs(today)
-        diffDays = Math.floor(((todayAbs - utcInput)/oneDay)); // get the difference between milliseconds and transforms to days
-       
+        diffDays = Math.floor(((todayAbs - utcInput)/oneDay)); // get the difference between milliseconds and transforms to days 
     }
     else if(yourSelect.options[ yourSelect.selectedIndex ].value == "ultrasound"){
         const inputUltraDate = document.getElementById('dateUltra').value
@@ -49,11 +49,9 @@ function calculate(){
         const utcInput = new Date(dateUltra).getTime()
         const weekAdd = parseInt(document.getElementById('weeks').value)*7*24 * 60 * 60 * 1000
         const dayAdd = (document.getElementById('days').value)*24 * 60 * 60 * 1000
-        const today = new Date().getTime()
-        const todayAbs = Math.abs(today)
         diffDays = Math.floor(((todayAbs - utcInput)+(weekAdd + dayAdd))/oneDay);
-        
     }
+
     weeksAge = Math.floor(diffDays/7)
     daysAge = (diffDays%7)
     alert(`A idade gestacional é de ${weeksAge} semanas e ${daysAge} dias`)
