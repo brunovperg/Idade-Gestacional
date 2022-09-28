2ecc536fa862405c29fe2f6bd07496974c3a448f
Bruno Pergentino <brunovpergentino@gmail.com>
2022-09-28 14:40:17 -0300

    trying to correct the UTC time, test

--- main.js
+++ main.js
@@ -39,14 +39,14 @@ HidebothForm();
 const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
 function calculate(){
     const yourSelect = document.getElementById( "methods" );
-    const today = new Date().getTime() //gets today's date and transforms into milliseconds
-    const todayAbs = Math.abs(today)
+    const today = new Date().toUTCString() //gets today's date and transforms into milliseconds
+    const todayAbs = Math.abs(new Date(today).getTime())
 
     if(yourSelect.options[ yourSelect.selectedIndex ].value == "menst"){
         const inputMenstDate = document.getElementById('dateMenst').value //get input from html
         const dateMenst = new Date(inputMenstDate).toUTCString() //transform input into midnight on the day picked
         const utcInput = new Date(dateMenst).getTime()//transforms date into milliseconds 
-        diffDays = Math.floor(((todayAbs - utcInput)/oneDay))-1;// get the difference between milliseconds and transforms to days 
+        diffDays = Math.floor(((todayAbs - utcInput)/oneDay));// get the difference between milliseconds and transforms to days 
     }
     else if(yourSelect.options[ yourSelect.selectedIndex ].value == "ultrasound"){
         const inputUltraDate = document.getElementById('dateUltra').value
@@ -54,7 +54,7 @@ function calculate(){
         const utcInput = new Date(dateUltra).getTime()
         const weekAdd = parseInt(document.getElementById('weeks').value)*7*24 * 60 * 60 * 1000
         const dayAdd = (document.getElementById('days').value)*24 * 60 * 60 * 1000
-        diffDays = Math.floor(((todayAbs - utcInput)+(weekAdd + dayAdd))/oneDay)-1;
+        diffDays = Math.floor(((todayAbs - utcInput)+(weekAdd + dayAdd))/oneDay);
     }
 
     weeksAge = Math.floor(diffDays/7)
